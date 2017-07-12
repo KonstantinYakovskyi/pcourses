41d74c823ac25694fd518761a7b83e1f20910d25
PLARIUM\k.yakovskyi <k.yakovskyi@plarium.com>
2017-07-12 10:16:17 +0300

    add xjoin()

---
+++ Practice1/xjoin.py
@@ -0,0 +1,14 @@
+def xjoin():
+    # array = raw_input("Enter array: ")
+    # s = raw_input("Enter s: ")
+
+    array = ['is', 'the', 'capital', 'of', 'GB']
+    s = 'London'
+
+    for i in array:
+        s += '-' + str(i)
+    print s
+
+
+if __name__ == "__main__":
+    exit(xjoin())
