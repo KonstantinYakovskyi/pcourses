c7f3db878d9e73fb531fa8684ee7a461ed08960e
Anastasia Grigorova <grigorovastasia@gmail.com>
2017-07-17 19:08:33 +0300

    add decode

--- Practice1/decode.py
+++ Practice1/decode.py
@@ -1,20 +1,44 @@
-from helper import find_index
 def decode():
-    row = raw_input("Enter row to decode: ")
+    # row = raw_input("Enter row to decode: ")
+    rows = ['', '1', '11', '11111', '11#', '11##', '11122234###55']
 
-    result = ''
-    for i in range(len(row)):
-        counter = 0
-        if row[i] == row[i+1] and row[i] != '#':
-            while row[i] == row[i+1] and row[i] != '#':
-                counter +=1
-                i +=1
-                result += row[i]
-                i = i + counter -1
+    def func(rows):
+        actual_result = []
+        for row in rows:
+            result = ''
+            previous_symbol = ''
 
-    if len(find_index(row, '#')) > 1:
-        result = result + result[-1:]
-    print('Result is: ' + result)
+            for current_symbol in row:
+                last_symbol_in_result = result[-1:]
+                penultimate_symbol_in_result = result[-2:-1]
+
+                if previous_symbol == '':
+                    previous_symbol = current_symbol
+                    continue
+
+                elif current_symbol == previous_symbol:
+                    # check if '#' is already executed
+                    if current_symbol == '#':
+                        if penultimate_symbol_in_result == last_symbol_in_result:
+                            continue
+                        else:
+                            result += last_symbol_in_result
+
+                    elif last_symbol_in_result != current_symbol:
+                        result += current_symbol
+                previous_symbol = current_symbol
+
+            actual_result.append(result)
+        return actual_result
+
+    expected_results = ['', '', '1', '1', '1', '11', '1225']
+    actual_results = func(rows)
+
+    assert func(rows) == expected_results
+
+    print(actual_results == expected_results)
+    print('Expected results: ' + str(expected_results))
+    print('Actual results: ' + str(actual_results))
 
 
 if __name__ == "__main__":
