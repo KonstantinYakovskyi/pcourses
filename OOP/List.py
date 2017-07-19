96e951fdc64ea19061a71acd9a79078ac65f659e
Anastasia Grigorova <grigorovastasia@gmail.com>
2017-07-19 18:41:37 +0300

    add comments

--- OOP/List.py
+++ OOP/List.py
@@ -1,4 +1,3 @@
-# https://stackoverflow.com/questions/3207219/how-do-i-list-all-files-of-a-directory
 import fnmatch
 
 import cherrypy
@@ -7,26 +6,16 @@ from os.path import isfile, join
 
 
 class DirListener:
-    def __init__(self, root):
-        self.root = root
-
+    @cherrypy.expose
     def index(self):
         return 'hi there. select /list or /txt_list'
 
-    def list(self):
-        result = [f for f in listdir(self.root) if isfile(join(self.root, f))]
+    @cherrypy.expose
+    def list(self, root):
+        result = [f for f in listdir(root) if isfile(join(root, f))]
         return str(result)
 
-
-class TxtListener(DirListener):
-    def txt_list(self):
-        lst = self.list()
-        res = [file for file in lst if fnmatch.fnmatch(file, '*.txt')]
-        return res
-
-
 h1 = DirListener()
-
 if __name__ == '__main__':
-    cherrypy.quickstart(expose)
+    cherrypy.quickstart(h1)
 
---
+++ OOP/ListInheritance.py
@@ -0,0 +1,31 @@
+import fnmatch
+
+import cherrypy
+from os import listdir
+from os.path import isfile, join
+
+
+class DirListener:
+    def __init__(self, root):
+        self.root = root
+
+    def index(self):
+        return 'hi there. select /list or /txt_list'
+
+    def list(self):
+        result = [f for f in listdir(self.root) if isfile(join(self.root, f))]
+        return str(result)
+
+
+class TxtListener(DirListener):
+    def txt_list(self):
+        lst = self.list()
+        res = [file for file in lst if fnmatch.fnmatch(file, '*.txt')]
+        return res
+
+
+h1 = DirListener()
+
+if __name__ == '__main__':
+    cherrypy.quickstart(expose)
+
